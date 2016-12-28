8232fb14fbf9fedc1d837957f136662850c40c07
remario <remariorich@gmail.com>
2016-12-27 22:02:54 -0600

    finished login layout

--- app/src/main/java/com/palisadoes/doroad/authentication/LoginActivity.java
+++ app/src/main/java/com/palisadoes/doroad/authentication/LoginActivity.java
@@ -3,6 +3,8 @@ package com.palisadoes.doroad.authentication;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.palisadoes.doroad.R;
+
 /**
  * Created by root on 12/27/16.
  */
@@ -11,5 +13,6 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
     }
 }
