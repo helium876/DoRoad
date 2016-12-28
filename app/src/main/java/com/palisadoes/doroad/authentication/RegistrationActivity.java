dd35c518797837b6929b0e28394e35b9fdd62f88
remario <remariorich@gmail.com>
2016-12-27 22:24:04 -0600

    finished registration layout

--- app/src/main/java/com/palisadoes/doroad/authentication/RegistrationActivity.java
+++ app/src/main/java/com/palisadoes/doroad/authentication/RegistrationActivity.java
@@ -3,6 +3,9 @@ package com.palisadoes.doroad.authentication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.palisadoes.doroad.R;
 
@@ -11,14 +14,41 @@ import com.palisadoes.doroad.R;
  */
 
 public class RegistrationActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "Droid";
+    Button register_button;
+    TextView login_account;
+    EditText email, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+        register_button = (Button) findViewById(R.id.btn_register);
+        login_account = (TextView) findViewById(R.id.link_login);
+        email = (EditText) findViewById(R.id.input_email);
+        password = (EditText) findViewById(R.id.input_password);
+
+        register_button.setOnClickListener(this);
+        login_account.setOnClickListener(this);
+        email.setOnClickListener(this);
+        password.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.btn_register:{
+                break;
+            }
+            case R.id.input_email:{
+                break;
+            }
+            case R.id.input_password:{
+                break;
+            }
+            case R.id.link_login:{
+                break;
+            }
+        }
     }
 }
