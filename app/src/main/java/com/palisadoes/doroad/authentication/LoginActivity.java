44f0967117893633febacbd8e0789dcd2f9dbc43
remario <remariorich@gmail.com>
2016-12-27 21:44:13 -0600

    added login activity,added to manifest file

---
+++ app/src/main/java/com/palisadoes/doroad/authentication/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.palisadoes.doroad.authentication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 12/27/16.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
