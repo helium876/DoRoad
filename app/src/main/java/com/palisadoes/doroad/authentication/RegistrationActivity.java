ff16d08fab2814ce75b30a28f2d45a147f83beaa
remario <remariorich@gmail.com>
2016-12-27 22:13:17 -0600

    add registration layout and activity,also modified manifest to include new activity

---
+++ app/src/main/java/com/palisadoes/doroad/authentication/RegistrationActivity.java
@@ -0,0 +1,24 @@
+package com.palisadoes.doroad.authentication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.palisadoes.doroad.R;
+
+/**
+ * Created by root on 12/27/16.
+ */
+
+public class RegistrationActivity extends Activity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_form);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
