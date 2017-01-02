e7810e99c3f005a49b25f9061ad5f88492d48b18
remario <remariorich@gmail.com>
2017-01-02 17:58:57 -0600

    added register activity and back navigation

--- app/src/main/java/com/example/remario/doroadv1/activities/MainActivity.java
+++ app/src/main/java/com/example/remario/doroadv1/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.remario.doroadv1;
+package com.example.remario.doroadv1.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.remario.doroadv1.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/remario/doroadv1/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.remario.doroadv1.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by remario on 1/2/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
