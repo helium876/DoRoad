20570327eb5a190cb33be1e91b5546657d6df08f
remario <remariorich@gmail.com>
2017-01-02 20:57:16 -0600

    added register method

--- app/src/main/java/com/example/remario/doroadv1/activities/RegisterActivity.java
+++ app/src/main/java/com/example/remario/doroadv1/activities/RegisterActivity.java
@@ -2,32 +2,40 @@ package com.example.remario.doroadv1.activities;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.remario.doroadv1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import constants.Constants;
 
 /**
  * Created by remario on 1/2/17.
  */
 
-public class RegisterActivity extends AppCompatActivity  implements View.OnClickListener{
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     private ProgressDialog progressDialog;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    private TextInputLayout email_wrapper,password_wrapper,name_wrapper;
+    private TextInputLayout email_wrapper, password_wrapper, name_wrapper;
     TextView link_login;
     Button register;
-    EditText email,password,name;
+    EditText email, password, name;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,7 +57,7 @@ public class RegisterActivity extends AppCompatActivity  implements View.OnClick
 
         register.setOnClickListener(this);
         link_login.setOnClickListener(this);
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -63,12 +71,32 @@ public class RegisterActivity extends AppCompatActivity  implements View.OnClick
         return super.onOptionsItemSelected(item);
     }
 
-    private void registerUser()
-    {
+    private void registerUser() {
+        String name_,email_,password_;
+        name_ = name.getText().toString();
+        email_ = email.getText().toString();
+        password_ = password.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email_, password_)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(Constants.LOGGER, "createUserWithEmail:onComplete:" + task.isSuccessful());
 
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
-    private void showProgressDialog(){
-      progressDialog = new ProgressDialog(RegisterActivity.this,
+
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog(RegisterActivity.this,
                 R.style.AppTheme_PopupOverlay);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating...");
@@ -77,16 +105,16 @@ public class RegisterActivity extends AppCompatActivity  implements View.OnClick
 
     @Override
     public void onClick(View v) {
-            switch (v.getId())
-            {
-                case R.id.link_login:{
-                    break;
-                }
-                case R.id.btn_signup:{
-                    registerUser();
-                    break;
-                }
-                default:break;
+        switch (v.getId()) {
+            case R.id.link_login: {
+                break;
             }
+            case R.id.btn_signup: {
+                registerUser();
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
