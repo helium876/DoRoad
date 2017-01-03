d3c428f44aae17e43727c84d00ef79eab5b71c3c
remario <remariorich@gmail.com>
2017-01-02 19:21:33 -0600

    added up navigation for register activity

--- app/src/main/java/com/example/remario/doroadv1/activities/RegisterActivity.java
+++ app/src/main/java/com/example/remario/doroadv1/activities/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.example.remario.doroadv1.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.remario.doroadv1.R;
 
 /**
  * Created by remario on 1/2/17.
@@ -12,5 +17,23 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
